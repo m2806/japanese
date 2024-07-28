11b5a8c6fc3925d605c3da82bdf4a2471d7783cf
m2806 <programmer286@protonmail.com>
2024-07-29 00:01:16 +0900

    Added different layout for landscape and portrate

--- static/vocabulary drill script.js
+++ static/vocabulary drill script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 	
-	const tbodies = document.getElementsByTagName('tbody');
+	const tbodies = document.getElementById('data').getElementsByTagName('tbody');
 	const tbodies_length = tbodies.length;
 	const generate_random_index = () => Math.floor(Math.random() * tbodies_length) + 1;
 	const generate_data_kanji_index = (integer) => `#row_index_${integer} .data_kanji`;
@@ -23,61 +23,77 @@ document.addEventListener('DOMContentLoaded', function() {
 	let english_elements;
 	let spanish_elements;
 	
-	function display_kanji (integer) {
+	let portrait_table = document.getElementById('portrait_table');
+	const original_portrait_table_innerHTML = document.getElementById('portrait_table').innerHTML;
+	
+	function display_question (integer) {
+		kanji_index = generate_data_kanji_index (integer);
+		
+		//Landscape table
+		document.querySelector('#landscape_question_body').style.display = 'table-row-group';
+		document.querySelector('#landscape_answer_body').style.display = 'none';
+		document.querySelector('#landscape_kanji_field').textContent = document.querySelector(kanji_index).textContent;
+		
+		//Portrait table
+		portrait_table.innerHTML = original_portrait_table_innerHTML;
+		document.querySelector('#portrait_kanji_data').textContent = document.querySelector(kanji_index).textContent;
+		document.querySelector('#portrait_kana_header').setAttribute('rowspan', 1);
+		document.querySelector('#portrait_english_header').setAttribute('rowspan', 1);
+		document.querySelector('#portrait_spanish_header').setAttribute('rowspan', 1);
+	}
+	
+	function display_answer (integer) {
 		tbody_index = generate_tbody_index (integer);
 		kanji_index = generate_data_kanji_index (integer);
 		kana_index = generate_data_kana_index (integer);
 		english_index = generate_data_english_index (integer);
 		spanish_index = generate_data_spanish_index (integer);
-		document.querySelector(tbody_index).style.display = 'table-row-group';
-		document.querySelector(kanji_index).style.display = 'table-cell';
 		kana_elements = document.querySelectorAll(kana_index);
 		english_elements = document.querySelectorAll(english_index);
 		spanish_elements = document.querySelectorAll(spanish_index);
-		kana_elements.forEach(function(element) {
-			element.style.display = 'none';
-		});
-		english_elements.forEach(function(element) {
-			element.style.display = 'none';
-		});
-		spanish_elements.forEach(function(element) {
-			element.style.display = 'none';
-		});
+		
+		//Landscape table
+		document.querySelector('#landscape_question_body').style.display = 'none';
+		document.querySelector('#landscape_answer_body').style.display = 'table-row-group';
+		document.querySelector('#landscape_answer_body').innerHTML = document.querySelector (tbody_index).innerHTML;
+		
+		//Portrait table
+		document.querySelector('#portrait_kana_header').setAttribute('rowspan', kana_elements.length);
+		document.querySelector('#portrait_english_header').setAttribute('rowspan', english_elements.length);
+		document.querySelector('#portrait_spanish_header').setAttribute('rowspan', spanish_elements.length);
+		for (let i = 0; i < kana_elements.length; i++) {
+            if (i == 0) {
+				document.querySelector('#portrait_kana_data').textContent += kana_elements [i].innerHTML;
+			} else {
+				document.querySelector('#portrait_kana_body').innerHTML += `<tr><td>${kana_elements[i].innerHTML}</td></tr>`;
+			}
+        }
+		for (let i = 0; i < english_elements.length; i++) {
+            if (i == 0) {
+				document.querySelector('#portrait_english_data').textContent += english_elements [i].innerHTML;
+			} else {
+				document.querySelector('#portrait_english_body').innerHTML += `<tr><td>${english_elements[i].innerHTML}</td></tr>`;
+			}
+        }
+		for (let i = 0; i < spanish_elements.length; i++) {
+            if (i == 0) {
+				document.querySelector('#portrait_spanish_data').textContent += spanish_elements [i].innerHTML;
+			} else {
+				document.querySelector('#portrait_spanish_body').innerHTML += `<tr><td>${spanish_elements[i].innerHTML}</td></tr>`;
+			}
+        }
 	}
 	
-	function display_tbody (integer) {
-		tbody_index = generate_tbody_index (integer);
-		kanji_index = generate_data_kanji_index (integer);
-		kana_index = generate_data_kana_index (integer);
-		english_index = generate_data_english_index (integer);
-		spanish_index = generate_data_spanish_index (integer);
-		document.querySelector(tbody_index).style.display = 'table-row-group';
-		document.querySelector(kanji_index).style.display = 'table-cell';
-		kana_elements = document.querySelectorAll(kana_index);
-		english_elements = document.querySelectorAll(english_index);
-		spanish_elements = document.querySelectorAll(spanish_index);
-		kana_elements.forEach(function(element) {
-			element.style.display = 'table-cell';
-		});
-		english_elements.forEach(function(element) {
-			element.style.display = 'table-cell';
-		});
-		spanish_elements.forEach(function(element) {
-			element.style.display = 'table-cell';
-		});
-	}
-	
-	display_kanji (integer);
+	display_question (integer);
 	
 	document.querySelector('#next').onclick = function() {
 		if (flow_state) { //show answer -> show question
 			flow_state = false;
-			document.querySelector(tbody_index).style.display = 'none';
 			integer = generate_random_index();
-			display_kanji (integer);
+			display_question (integer);
 		} else { //show question -> show answer
 			flow_state = true;
-			display_tbody (integer);
+			display_answer(integer);
 		}
 	};
 });
